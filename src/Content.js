880a1b4930bda7ac53437ad657ad1daa089eca71
Mississippi Brennan <mbrennan@blueorigin.com>
2023-06-13 15:39:47 -0700

    end chapter 6

--- src/Content.js
+++ src/Content.js
@@ -1,33 +1,44 @@
-import React from 'react'
+import { useState } from 'react';
+import React from 'react';
 
 const Content = () => {
+    const [name, setName] = useState('Dave');
+
+    const [count, setCount] = useState(0);
+
     const handleNameChange = () => {
     const names = ['Bob', 'Kevin', 'Dave'];
     const int = Math.floor(Math.random() * 3)
-    return names[int];
+    setName(names[int]);
   }
 
   const handleClick =()=> 
   {
-    console.log('You clicked it')
+    setCount(count+1);
+     setCount(count+1);
+    console.log(count)
   }
 
-  const handleClick2 =(name)=> 
+  const handleClick2 =()=> 
   {
-    console.log(`${name} was clicked`)
+    console.log(count)
   }
-  const handleClick3 =(e)=> 
-  {
-    console.log(e.target.innerText)
-  }
+
+  // const handleClick3 =(e)=> 
+  // {
+  //   console.log(e.target.innerText)
+  // }
   return (
    <main>
-    <p onDoubleClick={handleClick}> Hello {handleNameChange()}!</p> 
+    <p onDoubleClick={handleClick}> Hello {name}!</p> 
+   <button onClick={handleNameChange}>Change Name</button>
+
    <button onClick={handleClick}>Click It</button>
-   <button onClick={() => {handleClick2('Dave')}}>Click It</button>
-   <button onClick={(e) => {handleClick3(e)}}>Click It</button>
+
+   <button onClick={handleClick2}>Click It</button>
    </main>
   )
 }
 
 export default Content
+
