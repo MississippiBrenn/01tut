97b670ae1d919988abbf941824ba7a21c2bb5458
Mississippi Brennan <mbrennan@blueorigin.com>
2023-07-06 16:54:47 -0700

    ch 9

---
+++ src/SearchItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SearchItem = ({ search, setSearch }) => {
+  return (
+    //won't reload page on enter 
+    <form className = 'searchForm' onSubmit={(e) => e.preventDefault()}>
+    <label htmlFor="search">Search</label>
+    <input id='search' type="text" role='searchbox' placeholder='Search Items'
+    value={search}
+    onChange={(e)=> setSearch(e.target.value)}/>
+    </form>
+  )
+}
+
+export default SearchItem
